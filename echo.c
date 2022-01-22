3e03b5d45bb37df2c69aff8b6b332a353f094fce
Vito Gamberini <vito@gamberini.email>
2022-01-22 06:45:04 -0500

    Refactor echo.c

--- echo.c
+++ echo.c
@@ -1,10 +1,7 @@
-#include "stat.h"
 #include "user.h"
 
 int main(int argc, char* argv[]) {
-  int i;
-
-  for(i = 1; i < argc; i++)
+  for(int i = 1; i < argc; i++)
     printf(1, "%s%s", argv[i], i + 1 < argc ? " " : "\n");
   exit();
 }
