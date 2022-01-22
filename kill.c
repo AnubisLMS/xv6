7fbf1c1a829840af0417156ce59df503e0fb0a2b
Vito Gamberini <vito@gamberini.email>
2022-01-22 10:26:47 -0500

    Refactor kill.c

--- kill.c
+++ kill.c
@@ -1,14 +1,11 @@
-#include "stat.h"
 #include "user.h"
 
 int main(int argc, char** argv) {
-  int i;
-
   if(argc < 2) {
     printf(2, "usage: kill pid...\n");
     exit();
   }
-  for(i = 1; i < argc; i++)
+  for(int i = 1; i < argc; i++)
     kill(atoi(argv[i]));
   exit();
 }
