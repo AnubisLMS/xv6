e712737f768443eed9facc59607b7036cc80cfe7
Vito Gamberini <vito@gamberini.email>
2022-01-22 05:43:27 -0500

    Refactor cat.c

--- cat.c
+++ cat.c
@@ -8,6 +8,7 @@ void cat(int fd) {
 
   while((n = read(fd, buf, sizeof(buf))) > 0)
     write(1, buf, n);
+
   if(n < 0) {
     printf(1, "cat: read error\n");
     exit();
@@ -15,14 +16,13 @@ void cat(int fd) {
 }
 
 int main(int argc, char* argv[]) {
-  int fd, i;
-
   if(argc <= 1) {
     cat(0);
     exit();
   }
 
-  for(i = 1; i < argc; i++) {
+  for(int i = 1; i < argc; i++) {
+    int fd;
     if((fd = open(argv[i], 0)) < 0) {
       printf(1, "cat: cannot open %s\n", argv[i]);
       exit();
